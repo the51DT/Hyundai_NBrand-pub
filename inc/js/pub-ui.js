80af58be610a2184ff3fad17125fa9120eb1d893
100minkyung <mkbaek@the-51.com>
2024-09-30 16:32:58 +0900

    컨피규레이터, 국가선택, 좋아요버튼 수정
    컨피규레이터 이미지 전환 시 깜빡거리는 애니메이션 pub-ui.js 에서 주석처리
    
    국가 선택 하나만 있을 경우 <p>로 노출 주석 추가 EP010101_kr 파일에만 적용
    
    좋아요 버튼 숫자도 읽히게 aria-label 말고 title 값 안내 주석 EP040101_kr 파일에만 적용

--- inc/js/pub-ui.js
+++ inc/js/pub-ui.js
@@ -2166,12 +2166,14 @@ function configuratorEvent() {
       // configuratorImg();
 
       // 슬라이드 이미지 애니메이션
-      $(".configurator_swiper .swiper-slide img").hide();
       // $(".configurator_swiper .swiper-slide img").attr(
       //   "src",
       //   "../../inc/images/Configurator/NEN_EXT_XFB_LD_C003.jpg"
       // ); // 이미지 교체 확인용 임시 스크립트
-      $(".configurator_swiper .swiper-slide img").fadeIn(300);
+
+      // 이미지 변경하면 화면 깜빡임
+      // $(".configurator_swiper .swiper-slide img").animate({ opacity: 0.1 }, 200);
+      // $(".configurator_swiper .swiper-slide img").animate({ opacity: 1 }, 200);
     });
   });
 }
